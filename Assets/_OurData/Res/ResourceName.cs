decd668e5d9071cd4891882a91dd390c9f418426
SimonHome <tranminhduc18116@gmail.com>
2021-11-14 23:17:44 +0700

    E3

---
+++ Assets/_OurData/Res/ResourceName.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ResourceName
+{
+    //Money
+    gold = 1,
+    diamond = 2,
+
+    //Material Level 1
+    logwood = 1001,
+    ironOre = 1002,
+
+    //Material Level 2
+    blank = 2001,
+    ironIgnot = 2002,
+}
