f4820a509bc9fba201a2f69e7256a110fe0a2031
SimonHome <tranminhduc18116@gmail.com>
2021-11-21 04:07:28 +0700

    E-8

--- Assets/_OurData/Res/ResGenerator.cs
+++ Assets/_OurData/Res/ResGenerator.cs
@@ -2,21 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ResGenerator : MonoBehaviour
+public class ResGenerator : SaiBehaviour
 {
     [SerializeField] protected List<ResHolder> resHolders;
 
-    protected void Awake()
+    protected override void LoadComponents()
     {
         this.LoadHolders();
     }
 
     protected virtual void LoadHolders()
     {
+        if (this.resHolders.Count > 0) return;
+
         Transform res = transform.Find("Res");
         foreach (Transform resTran in res)
         {
             Debug.Log(resTran.name);
+            ResHolder resHolder = resTran.GetComponent<ResHolder>();
+            if (resHolder == null) continue;
+            this.resHolders.Add(resHolder);
         }
+
+        Debug.Log(transform.name + ": LoadHolders");
     }
 }
--- Assets/_OurData/Res/ResHolder.cs
+++ Assets/_OurData/Res/ResHolder.cs
@@ -2,20 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ResHolder : MonoBehaviour
+public class ResHolder : SaiBehaviour
 {
     [SerializeField] protected ResourceName resourceName;
     [SerializeField] protected float resCurrent = 0;
     [SerializeField] protected float resMax = Mathf.Infinity;
 
-    private void Awake()
+    protected override void LoadComponents()
     {
         this.LoadResName();
     }
 
     protected virtual void LoadResName()
     {
+        if (this.resourceName != ResourceName.noResource) return;
+
         string name = transform.name;
         this.resourceName = ResNameParser.FromString(name);
+        Debug.Log(transform.name + ": LoadResName");
     }
 }
