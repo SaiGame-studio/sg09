6ea837898991e5ac79749ecbc83be8baeadcc09e
SimonHome <tranminhduc18116@gmail.com>
2021-11-24 04:11:05 +0700

    ready for e-10

---
+++ Assets/_OurData/Building/BuildLevel.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildLevel : SaiBehaviour
+{
+    [SerializeField] protected List<Transform> levels;
+    [SerializeField] protected int currentLevel = 0;
+
+    private void OnEnable()
+    {
+        this.ShowBuilding();
+        InvokeRepeating("ShowNextBuild", 3, 3);
+    }
+
+    protected override void LoadComponents()
+    {
+        this.LoadLevels();
+    }
+
+    protected virtual void LoadLevels()
+    {
+        if (this.levels.Count > 0) return;
+        Transform buildTran = transform.Find("Buildings");
+        foreach (Transform child in buildTran)
+        {
+            this.levels.Add(child);
+            child.gameObject.SetActive(false);
+        }
+
+        Debug.Log(transform.name + ": LoadBuildings");
+    }
+
+    protected virtual void ShowNextBuild()
+    {
+        if (this.currentLevel >= this.levels.Count - 1) return;
+
+        this.currentLevel++;
+        this.ShowBuilding();
+    }
+
+    protected virtual void ShowBuilding()
+    {
+        this.HideLastBuild();
+        Transform currentBuild = this.levels[this.currentLevel];
+        currentBuild.gameObject.SetActive(true);
+    }
+
+    protected virtual void HideLastBuild()
+    {
+        int lastBuildIndex = this.currentLevel - 1;
+        if (lastBuildIndex < 0) return;
+        Transform lastBuild = this.levels[lastBuildIndex];
+        lastBuild.gameObject.SetActive(false);
+    }
+}
